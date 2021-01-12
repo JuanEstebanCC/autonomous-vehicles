88d7219053cff6f9cd81aaca5ee6b0c54dca01b7
JuanEstebanCC <juanescifuentes75@gmail.com>
2021-01-11 22:26:21 -0500

    Fase 2

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var carroEnMovimiento = false;
 var destinoElegido = false;
+var tiempoDeParada = Math.floor(Math.random() * 10 + 5);
+var numParadas = Math.floor(Math.random() * 3 + 1);
+var distanciaRecorrido = Math.floor(Math.random() * 1000 + 1);
+var tiempoParada = Math.floor(Math.random() * 5 + 1);
 class vehiculo {
   constructor(
     estacionaria,
@@ -23,7 +27,7 @@ class vehiculo {
     this.freno = freno;
   }
   estadoVehiculoOn() {
-    if (carro.frenoDeMano == false && carro.numCambio == 0) {
+    if (carro.frenoDeMano == true && carro.numCambio == 0) {
       document.getElementById("tablero").innerHTML += "<br>Vehiculo encendido!";
       document.getElementById("tablero").innerHTML += ``;
       console;
@@ -34,7 +38,7 @@ class vehiculo {
       ).innerHTML += `<br>¡El vehículo no se pudo encender!, revise que este en neutro, su cambio actual es: ${carro.numCambio}.`;
     } else {
       document.getElementById("tablero").innerHTML +=
-        "<br>¡El vehículo no se pudo encender!, que no tenga el freno de mano.";
+        "<br>¡El vehículo no se pudo encender!, no tiene el freno de mano puesto.";
     }
   }
 
@@ -59,19 +63,20 @@ class vehiculo {
       document.getElementById(
         "tablero"
       ).innerHTML = `<br> Calculando distancia.... `;
-      let numParadas = Math.floor(Math.random() * 3 + 1);
-      let distanciaRecorrido = Math.floor(Math.random() * 1000 + 1);
-      let tiempoParada = Math.floor(Math.random() * 5 + 1);
       function kmPerParada(distanciaRecorrido, numParadas, tiempoParada) {
         let cadaParadakilometros = Math.floor(
           Math.random() * distanciaRecorrido + 1
         );
-        document.getElementById(
-          "tablero"
-        ).innerHTML += `<br>Información sobre su nuevo destino: <br> -Tiene <b>${numParadas}</b> <b>parada/s.</b><br> -Una <b> distancia </b> para recorrer de <br><b>${distanciaRecorrido} kilómetros</b>. <br> -Un <b> tiempo</b> de parada de <b> ${tiempoParada} segundo/s</b>. <br>  -Y a los<b> ${cadaParadakilometros} kilómetros</b>  se <b>ejecutará </b>la parada.`;
+        setTimeout(function () {
+          document.getElementById(
+            "tablero"
+          ).innerHTML += `<br>Información sobre su nuevo destino: <br> -Tiene <b>${numParadas}</b> <b>parada/s.</b><br> -Una <b> distancia </b> para recorrer de <br><b>${distanciaRecorrido} kilómetros</b>. <br> -Un <b> tiempo</b> de parada de <b> ${tiempoParada} segundo/s</b>. <br>  -Y a los<b> ${cadaParadakilometros} kilómetros</b>  se <b>ejecutará </b>la parada.`;
+        }, 2000);
       }
       kmPerParada(distanciaRecorrido, numParadas, tiempoParada);
-      return (destinoElegido = true);
+      return (
+        (destinoElegido = true), distanciaRecorrido, numParadas, tiempoParada
+      );
     } else {
       document.getElementById(
         "tablero"
@@ -108,26 +113,42 @@ class vehiculo {
       carro.numCambio == 1 &&
       destinoElegido == true
     ) {
-      document.getElementById(
-        "tablero"
-      ).innerHTML = `<br> Freno de piso soltado! `;
-      document.getElementById(
-        "tablero"
-      ).innerHTML += `<br> ¡Usted ha arrancado correctamente! `;
-      document.getElementById(
-        "tablero"
-      ).innerHTML += `<br> Soltando clutch... `;
-      carro.freno = false;
       setTimeout(function () {
         document.getElementById(
           "tablero"
-        ).innerHTML += `<br> ¡Clutch soltado! `;
-        carro.clutch = false;
+        ).innerHTML = `<br> Comenzando arranque automático... `;
+      }, 800);
+      setTimeout(function () {
+        document.getElementById(
+          "tablero"
+        ).innerHTML = `<br> Freno de piso soltado! `;
       }, 1500);
+      setTimeout(function () {
+        document.getElementById(
+          "tablero"
+        ).innerHTML += `<br> ¡Usted ha arrancado correctamente! `;
+      }, 3500);
+      setTimeout(function () {
+        document.getElementById(
+          "tablero"
+        ).innerHTML += `<br> Soltando clutch... `;
+      }, 4500);
+      carro.freno = false;
 
+      document.getElementById("tablero").innerHTML += `<br> ¡Clutch soltado! `;
+      carro.clutch = false;
+      setTimeout(function () {
+        document.getElementById(
+          "tablero"
+        ).innerHTML += `<br> Auto en movimiento... `;
+      }, 5500);
       function presionar_tecla() {
         var tecla = event.keyCode;
-
+        setTimeout(function () {
+          document.getElementById(
+            "tablero"
+          ).innerHTML = `<br> Subiendo a sexta...`;
+        }, 6000);
         if (tecla == 76) {
           return carro.girarIzquierda();
         } else if (tecla == 82) {
@@ -139,7 +160,12 @@ class vehiculo {
         document.getElementById(
           "tablero"
         ).innerHTML = `<br> Presione la tecla L si desea ir a la izquierda, o presione R si desea ir a la derecha.`;
-      }, 2000);
+      }, 7500);
+
+      let tiempoAleatorio = Math.floor(Math.random() * 25000 + 10000);
+      setTimeout(function () {
+        carro.frenadoAletorio();
+      }, tiempoAleatorio);
       return (carroEnMovimiento = true), carro.clutch;
     } else if (destinoElegido == false) {
       document.getElementById(
@@ -205,7 +231,9 @@ class vehiculo {
     return (carro.estacionaria = true);
   }
   frenadoAletorio() {
-    if (carro.numCambio > 2 && carro.numCambio < 6 && carro.freno == true) {
+    carro.freno = true;
+    carro.numCambio = 3;
+    if (carro.numCambio > 2 && carro.numCambio < 6) {
       carro.activarEstacionarias();
       if (carro.estado == true) {
         setTimeout(function () {
@@ -272,15 +300,16 @@ class vehiculo {
         .button-new-travel{
          opacity: 0.1;
 
-     }
-     .button-frenado-aleatorio{
-       opacity: 0.1;
-    }
+        }
+         .button-frenado-aleatorio{
+         opacity: 0.1;
+          }
           </style>
           
           <span id="countdown" class="count"></span>
           <img src="images/stop.png" alt="Stop" class="stop" />
         `);
+
           (function myLoop(i) {
             setTimeout(function () {
               document.getElementById("countdown").innerHTML = i;
@@ -331,60 +360,92 @@ class vehiculo {
             .button-new-travel{
              opacity: 1; !important
     
-         }
-         .button-frenado-aleatorio{
-           opacity: 1; !important
-        }
+            }
+             .button-frenado-aleatorio{
+             opacity: 1; !important
+             }
               </style> `;
                 setTimeout(function () {
                   document.getElementById(
                     "tablero"
-                  ).innerHTML = `<br> Arrancando de nuevo!`;
+                  ).innerHTML = `<br> Arrancando de nuevo...`;
                 }, 1000);
                 setTimeout(function () {
                   setTimeout(function () {
-                  document.getElementById(
-                    "tablero"
-                  ).innerHTML += `<br> ¡Ha llegado a su destino!`;
-                }, 1500);
+                    document.getElementById(
+                      "tablero"
+                    ).innerHTML += `<br> ¡Ha llegado a su destino!`;
+                  }, 2000);
                   setTimeout(function () {
                     carro.pisarFreno();
-                    setTimeout(function () {
-                      document.getElementById(
-                        "tablero"
-                      ).innerHTML += `<br> Si desea estacionar vaya en reverse, recuerde es el cambio número 7.`;
-                    }, 2500);
-                    if (carro.numCambio == 8) {
-                      setTimeout(function () {
-                        document.getElementById(
-                          "tablero"
-                        ).innerHTML = `<br> Parqueando...`;
-                      }, 2000);
-                      document.getElementById(
-                        "tablero"
-                      ).innerHTML += `<br> Vehículo parqueado!`;
 
-                      carroEnMovimiento = false;
+                    setTimeout(function () {
+                      for (let i = 0; i < 5; i++) {
+                        carro.cambiarNumCambio();
+                      }
                       document.getElementById(
                         "tablero"
-                      ).innerHTML += `<br> Freno de mano puesto!`;
-                      carro.frenoDeMano == true;
+                      ).innerHTML += `<br> ¡Reversa puesta!`;
                       setTimeout(function () {
                         document.getElementById(
                           "tablero"
-                        ).innerHTML += `<br>  Apagando vehiculo...`;
+                        ).innerHTML += `<br> Iniciando parqueo automático...
+                       `;
                       }, 1000);
-                      document.getElementById(
-                        "tablero"
-                      ).innerHTML += `<br>  Vehiculo apagado.`;
-                      carro.estado = false;
-                    }
+
+                      if (carro.numCambio == 8) {
+                        setTimeout(function () {
+                          document.getElementById(
+                            "tablero"
+                          ).innerHTML = `<br> Parqueando...`;
+                        }, 3000);
+                        setTimeout(function () {
+                          document.getElementById(
+                            "tablero"
+                          ).innerHTML += `<br> Vehículo parqueado!`;
+                        }, 5000);
+                        carroEnMovimiento = false;
+                        setTimeout(function () {
+                          document.getElementById(
+                            "tablero"
+                          ).innerHTML += `<br> Freno de mano puesto!`;
+                        }, 7000);
+                        carro.frenoDeMano == true;
+                        setTimeout(function () {
+                          document.getElementById(
+                            "tablero"
+                          ).innerHTML += `<br>  Apagando vehiculo...`;
+                        }, 9000);
+                        setTimeout(function () {
+                          document.getElementById(
+                            "tablero"
+                          ).innerHTML += `<br>  Vehiculo apagado.`;
+                          console.log(carro.estado);
+                          let tiempoLlegada = Math.floor(
+                            Math.random() * 200 + 1
+                          );
+                          carro.velocidad = distanciaRecorrido / tiempoLlegada;
+                          let tiempoQueTardo =
+                            tiempoParada * numParadas + tiempoLlegada;
+                          let velocidadMaxima = Math.floor(
+                            Math.random() * 90 + 30
+                          );
+                          document.getElementById(
+                            "tablero"
+                          ).innerHTML = `<br>  Datos arrojados por los sensores: <br>
+                            Velocidad apoximada: ${carro.velocidad.toFixed(2)} Km/h<br>
+                            Tiempo que tardó en llegar:  ${tiempoQueTardo} minutos<br>
+                            Velocidad máxima:   ${velocidadMaxima} Km/h`;
+                          carro.estado = false;
+                        }, 11000);
+                      }
+                    }, 2500);
                   }, 1000);
                 }, 2000);
-              } //  your code here
-              if (--i) myLoop(i); //  decrement i and call myLoop again if i > 0
+              }
+              if (--i) myLoop(i);
             }, 1000);
-          })(Math.floor(Math.random() * 10 + 1)); //  pass the number of iterations as an argument
+          })(tiempoDeParada);
         }, 1000);
       }
     } else if (carro.freno == false) {
